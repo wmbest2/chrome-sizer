60de8b4d19c59afeefd49045ddc781ac420af0f0
Bill Best <wmbest2@gmail.com>
2009-09-02 13:02:06 -0400

    Added Options

--- resizer.js
+++ resizer.js
@@ -1,6 +1,10 @@
 // Connects up a message listener when the page loads
 // This will receive the port.postMessage() from the extension
 
+function white_space(field)
+{
+     return field.replace(/^\s*|\s*$/g,'');
+}
 
 jQuery(document).ready(function() {
 
@@ -18,16 +22,31 @@ chrome.extension.onConnect.addListener(function(port) {
 
 console.log("init toolstrip");
 jQuery('.options').hide();
-	jQuery('.toolstrip-button').click(function() { 
+	jQuery('.main').click(function() { 
 	
 	chrome.toolstrip.expand({height:300});
 	jQuery('.options').show();
+	jQuery('.ext').width('200px');
+		
+	});
+
+	jQuery('.size').click(function(event) { 
+	console.log("size event initiated");
+	console.log("size: " + white_space(jQuery(event.target).text()));
+	var s = white_space(jQuery(event.target).text()).split('x');
+	console.log(s);
+	var h = s[1] * 1;
+	var w = s[0] * 1;
+	console.log( w + " by " + h);
+	port.postMessage({height:h, width:w});
 		
 	});
 
 	jQuery('.ext').bind("mouseleave", function() {
 		chrome.toolstrip.collapse();
 		jQuery('.options').hide();
+		jQuery('.ext').width('100%');
+		
 	});
 
 
